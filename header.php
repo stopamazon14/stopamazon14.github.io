72620a1436a0b75ab05a5e90591fd978c94d9f52
Mebrylla14 <59839442+Mebrylla14@users.noreply.github.com>
2021-03-03 11:12:28 +0100

    header php

---
+++ header.php
@@ -0,0 +1,15 @@
+<header>  
+  <div class="bandeau">
+    <a href="index.html"><img src='photos/fond.png'></a>
+  </div>
+  <div class="navbar">
+    <ul class="flex flex-center">
+      <li><a href="index.html">La une</a> </li>
+      <li><a href="moult.html">Le projet</a> </li>
+      <li><a href="mondeville.html">L'échec d'Amazon à Mondeville</a></li>
+      <li><a href="alternatives.html">Les alternatives</a></li>
+      <li><a href="revuepresse.html">Revue de presse</a></li>
+      <li><a href="stickers.html">Affiches/flyers</a></li>
+    </ul>
+  </div>
+</header> 
---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Stop Amazon 14</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="CSS_layout.css" media="screen" />
+  <link href="photos/favicon.ico" rel="icon" type="image/x-icon" />
+</head>
+<body>
+
+<?php include ('header.php'); ?>
+
+</div>
+<div class="container">
+  <div class="content">
+    <h2>Non à Amazon à Moult (14) ! Ni ici ni ailleurs.</h2>
+    <img src='photos/niMoult.png' class="img-resp float-r"/>
+    <p>Amazon veut implanter un entrepôt d’environ 8000 m2 (5600 m2 d’entrepôts + 2400 m2 en préau) sur près de 13 m de hauteur, sur un terrain de la commune de Moult-Chicheboville.</p>
+    <p>L’agence de Moult va recevoir, reconditionner et réexpédier une masse de marchandises. Cela va donc générer un trafic routier très important.</p>
+    <p>Il est prévu 8 quais de chargement pour poids lourds, ce qui implique au minimum un trafic d’une vingtaine de semi-remorques toutes les nuits et un ballet incessant de camionnettes le jour, car Amazon fonctionne de jour comme de nuit 24h/24H et ce 7j /7j. Sans compter les centaines de places de parkings pour les camionnettes.</p>
+    <p>On sait qu’Amazon détruit des emplois (cf le rapport de Mounir Mahjoubi en 2019), détruit les commerces de proximité, pratique la fraude fiscale à grande échelle et est un des pires émetteurs de CO2 au monde. Et même, la Convention Citoyenne sur le Climat a exigé un moratoire sur ce type d’implantations.</p>
+    <p>En décembre 2019, Mme Burgat la maire de Mondeville ainsi que les élus de Caen-la-Mer avaient pourtant refusé Amazon. Ils déclaraient qu’Amazon détruisait des emplois dans le commerce physique traditionnel, pratiquait l’évasion fiscale :</p>
+    <p class="italic">« pour se soustraire au système de solidarité qui régit notre pacte social républicain », et que « le volume d’achats, le stockage numérique des données, le développement de la livraison ultra-rapide impacteraient fortement l’environnement ».</p>
+    <br>
+    <p class="italic">Questions</p>
+    <br>
+    <h3>Pourquoi Mondeville et Caen-la-Mer ont refusé l’installation d’Amazon chez eux ?</h3>
+    <br>
+	<h3>Pourquoi des élus de droite comme de gauche ont fait front et ont réussi à empêcher ce que Moult est prêt à accepter sans le moindre débat public ?
+	</h3>
+	<br>
+    <h3>Faut-il laisser faire et s’apprêter à en subir les conséquences ?
+	OU
+	Réagir et réfléchir à notre intérêt commun ?
+	</h3>
+	<br>
+    <p>Amazon à Mondeville ou à Moult, l’impact sera le même : tout le territoire du Calvados et même de la Manche et de l’Orne en subiront les conséquences néfastes.</p>
+    <br>
+    <h3>Refusons Amazon à Moult en signant cette <a href="https://agir.greenvoice.fr/petitions/stop-amazon-moult-14">pétition</a></h3>
+    <br>
+    <p>Infos et contacts</p>
+    <p>stopamazon14@riseup.net</p>
+    <p><a href="https://www.facebook.com/stopamazon14">FB Stopamazon14</a></p>
+    <p><a href="https://stopamazon14.github.io/">Site Web Stopamazon14</a></p>
+    <p>06.85.73.26.41</p>
+    
+    <p class="italic">Consultons, témoignons, décidons et agissons maintenant !</p>
+    
+    
+    
+  </div>
+</div>
+
+      <!-- Footer, end of the webpage -->
+      <div class="footer">
+        <ul>
+          <li><a href="contact.html">Contact</a></li>
+          <li><a href="https://www.stop-amazon.fr/" target="_blank">Stop amazon France</a></li>
+        </ul>
+        <p> 2021 - Un site créé par <a href="https://www.facebook.com/14attac" target="_blank">Attac14 (FB)</a>, <a href="https://www.facebook.com/xrcaen" target="_blank">Exctinction Rebellion Caen (FB)</a>, <a href="https://www.facebook.com/stopamazon14/" target="_blank">FB stop amazon 14</a></p>
+
+      </div>
+
+    </body>
+    </html>
+
+
+
+
+
+
+
+
+
