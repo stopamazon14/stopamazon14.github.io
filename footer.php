08e11bdfd78126032cbacc7b6173cf26fa08a948
Mebrylla14 <59839442+Mebrylla14@users.noreply.github.com>
2021-03-03 12:03:21 +0100

    Fichiers php en HTML :'(

--- footer.php
+++
@@ -1,7 +0,0 @@
-<div class="footer">
-	<ul>
-		<li><a href="contact.html">Contact</a></li>
-		<li><a href="https://www.stop-amazon.fr/" target="_blank">Stop amazon France</a></li>
-	</ul>
-	<p> 2021 - Un site créé par <a href="https://www.facebook.com/14attac" target="_blank">Attac14 (FB)</a>, <a href="https://www.facebook.com/xrcaen" target="_blank">Exctinction Rebellion Caen (FB)</a>, <a href="https://www.facebook.com/stopamazon14/" target="_blank">FB stop amazon 14</a></p>
-</div>
